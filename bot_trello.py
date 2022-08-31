e49b957ab8ea216881740bd68d7fdf63917d55cb
deadpanda <clement.lagier@epitech.eu>
2022-08-31 12:43:36 +0200

    parse the first command : /add_card [boards] [cards to add]

--- bot_trello.py
+++ bot_trello.py
@@ -7,7 +7,26 @@ config = dotenv_values(".env")
 intents = discord.Intents.default()
 intents.message_content = True
 
+
+
+trello_client = TrelloClient(
+        api_key=config["API_KEY"],
+        api_secret=config["API_SECRET"],
+        token=config["TOKEN"]
+        )
+
 client = discord.Client(intents=intents)
+
+
+def check_if_good_format(message):
+    exploded_msg = (message.content).split(" ")
+    if len(exploded_msg) >= 3:
+        boards = exploded_msg[1]
+        #"..." check if the boards exists
+        cards_to_add = exploded_msg[2:]
+        return boards, cards_to_add
+    return None, None
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -19,4 +38,12 @@ async def on_message(message):
 
     if message.content.startswith("$hello"):
         await message.channel.send("Hello :)")
+    if message.content.startswith("/add_card"):
+        boards, cards = check_if_good_format(message)
+        if (boards and cards):
+            await message.channel.send("Added in {}: {}".format(boards, cards))
+        else:
+            await message.channel.send("Nope, your command must looking like this: /add_card [boards] [cards]")
+
+
 client.run(config["BOT_TOKEN"])
--- test_trello.py
+++ test_trello.py
@@ -3,21 +3,18 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env") # load var in the .env file
 
-client = TrelloClient(
-        api_key=config["API_KEY"],
-        api_secret=config["API_SECRET"],
-        token=config["TOKEN"]
-        )
 
+BOARD_ID = None
 current_boards = None
+is_board_closed = False
+
+LIST = "coucou"
 
 all_boards = client.list_boards()
 for boards in all_boards:
-    if boards.name == "Cpp Game":
+    if boards.name == "Cpp Game" and not boards.closed:
         current_boards = boards.name
-        break
-
-if not current_boards:
-    print("No boards found")
-else:
-    print("Found") 
+        BOARD_ID = boards.id 
+        all_lists = client.get_board(BOARD_ID).all_lists()
+        list_to_complete = [final_list for final_list in all_lists if final_list.name == LIST] 
+        card_to_add = list_to_complete[0].add_card(name="ceci est un test", desc="J'AI AJOUTÉ CETTE CARTE EN PYTHON")
